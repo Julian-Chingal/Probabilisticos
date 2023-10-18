3e87d882ffec5f86d17dc817f107099bdd12644e
Julian-Chingal <mauriciochingal16@gmail.com>
2023-10-18 01:58:34 -0500

    update

--- Entrega_2/NaiveBayes.py
+++ Entrega_2/NaiveBayes.py
@@ -3,49 +3,68 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split as tts
 from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder 
 from yellowbrick.classifier import confusion_matrix as cm
-#dataset
-from sklearn.datasets import load_wine
 
 #Variables
 label = LabelEncoder()
 model = GaussianNB()
+model2 = GaussianNB()
 
 # Data file
-path = os.path.abspath("Entrega_2/Data/water_potability.xlsx")    #https://www.kaggle.com/datasets/girishvutukuri/diabetes-binary-classification/
-srcfile = pd.read_excel(path)
+path = os.path.abspath("Entrega_2/Data/Titanic_survival.xlsx")    #https://www.kaggle.com/competitions/titanic/data?select=train.csv
+srcfile = pd.read_excel(path, header= 0)
 srcfile = srcfile.dropna()  # Eliminar instancias imcompletas
+srcfile['Sex'] = label.fit_transform(srcfile['Sex']) # Transformar a binario las etiquetas del sexo
 
-#data library
-dataRe = load_wine()
+path2 = os.path.abspath("Entrega_2/Data/water_potability.xlsx")    #https://www.kaggle.com/datasets/girishvutukuri/diabetes-binary-classification/
+srcfile2 = pd.read_excel(path2)
+srcfile2 = srcfile2.dropna()  # Eliminar instancias imcompletas
 
 #--------------------------------------------------------------------------------------------------------------------------------
-# Trainin file
-x = srcfile.drop(columns=['Potability']) #eliminar la variable objetivo para entrenar le modelo con los atributos
-y = srcfile['Potability'] # Variable objetivo
+# Trainin file water
+x = srcfile.drop(columns=['Survived']) #eliminar la variable objetivo para entrenar le modelo con los atributos
+y = srcfile['Survived'] # Variable objetivo
 x_train, x_test, y_train, y_test = tts(x, y, test_size=0.3, random_state=42) 
 
-#Train library
-x_train, x_test, y_train, y_test = tts(dataRe['data'], dataRe['target'], test_size=0.3, random_state=0) 
-#--------------------------------------------------------------------------------------------------------------------------------
+#train file Titanic Survival
+x2 = srcfile2.drop(columns=['Potability']) #eliminar la variable objetivo para entrenar le modelo con los atributos
+y2 = srcfile2['Potability'] # Variable objetivo
+x_train2, x_test2, y_train2, y_test2 = tts(x2, y2, test_size=0.3, random_state=42) 
 
-model.fit(x_train,y_train) #entrenar el modelo
+#--------------------------------------------------------------------------------------------------------------------------------
+# fit
+model.fit(x_train,y_train) #entrenar el modelo titanic
+model2.fit(x_train2,y_train2) #entrenar el modelo water
 
 #test
 prediction = model.predict(x_test) #Predicciones ustilziando el conjunto de prueba
 accuracy = accuracy_score(y_test, prediction) #precision del modelo
+print(f"Precision del modelo Titanic: {accuracy}\n---------------------------------")
 
-print(f"----------------------------------\nPrecision de la prediccion:\n----------------------------------\n{accuracy}\n----------------------------------")
+prediction2 = model2.predict(x_test2) #Predicciones ustilziando el conjunto de prueba
+accuracy2 = accuracy_score(y_test2, prediction2) #precision del modelo
+print(f"Precision del modelo Water: {accuracy2}\n---------------------------------")
 
-#graphic
-classes = ["No Potable", "Potable"]   #File
-classes = dataRe.target_names         #library
 
-Matriz= cm(model,x_train,y_train,x_test,y_test, classes=classes, cmap="Greens")
-Matriz.show()
 
-#porcentaje
-Matriz= cm(model,x_train,y_train,x_test,y_test, classes=classes, percent=True, cmap="Greens")
-Matriz.show()
+
+# graphic
+#Titanic
+Clases = ["Die","Survival"]
+Matriz= cm(model,x_train,y_train,x_test,y_test, classes= Clases,cmap="Greens")
+Matriz.set_title("Matriz de Confusión - Titanic")
+Matriz.poof()
+
+Matriz= cm(model,x_train,y_train,x_test,y_test,classes= Clases, percent=True, cmap="Greens")
+Matriz.set_title("Matriz de Confusión - Titanic")
+Matriz.poof()
+
+# Water
+Clases = ["No Potable","Potable"]
+Matriz2= cm(model2,x_train2,y_train2,x_test2,y_test2, classes= Clases,cmap="Greens")
+Matriz2.poof()
+
+Matriz2= cm(model2,x_train2,y_train2,x_test2,y_test2,classes= Clases, percent=True, cmap="Greens")
+Matriz2.show()
--- Entrega_2/NaiveBayes_manual.py
+++ Entrega_2/NaiveBayes_manual.py
@@ -1,4 +1,32 @@
-import os
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import model_selection as ms
+
+#Variables
+N_Attributes = 0
+media = 0
+sigma = 0
+pi = 0
+
+# Definicion del modelo
+def fit(Class, Data):
+    global N_Attributes,media, sigma,pi
+    
+    #Se inciializan las variables
+    N_Train_Instances, N_Attributes = Data.shape
+    N_Classes = np.max(Class) + 1
+
+    media, sigma = np.zeros((N_Classes,N_Attributes)), np.zeros((N_Classes, N_Attributes))
+    N_Instances = np.zeros(N_Classes)
+
+    for c in range(N_Classes):
+        Data_Class = Data[Class == c]  # Datos de entrada que pertenecen a una misma clase
+        N_Instances[c] = Data_Class.shape[0]
+        media [c,:] = np.mean(Data_Class,0)
+        sigma [c, :] = np.std(Data_Class, 0)
+    
+    pi = (N_Instances + 1) / (N_Train_Instances + N_Classes) 
+
+# suma exponencial de un conjunto de valores en la matriz 
+
+# Prediccion
